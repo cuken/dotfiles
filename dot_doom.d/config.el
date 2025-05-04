07f7436a20817d0b8dad2adae257956a4ac6def9
Cuken <cuken@cuken.dev>
2025-05-04 00:14:19 -0700

    chore(dotfiles): adding in emacs config

---
+++ dot_doom.d/config.el
@@ -0,0 +1,33 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+(setq user-full-name "Colin Uken"
+      user-mail-address "cuken@cuken.dev")
+
+(setq auth-sources '("~/.authinfo.gpg" "~/.authinfo")
+      auth-source-cache-expiry nil)
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;; (setq doom-font (font-spec :family "GeistMono Nerd Font" :size 15)
+;;      doom-variable-pitch-font (font-spec :family "Alegreya" :size 18)
+;;      doom-big-font (font-spec :family "GeistMono Nerd Font" :size 22))
+;;   presentations or streaming.
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
+;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+
+(setq doom-theme `doom-one)
+(setq display-line-numbers-type t)
+
+(setq doom-modeline-icon t)
+(setq doom-modeline-major-mode-icon t)
+(setq doom-modeline-lsp-icon t)
+(setq doom-modeline-major-mode-color-icon t)
+
+(set-frame-parameter (selected-frame) 'alpha '(96 . 97))
+(add-to-list 'default-frame-alist '(alpha . (96 . 97)))
+
+(setq org-direectory "~/org/")
+(setq org-hide-emphasis-markers t)
